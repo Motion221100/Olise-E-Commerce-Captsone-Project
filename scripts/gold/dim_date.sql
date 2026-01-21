baad2133a64c41660e0fa4558db15b9366d1c4c5
Keith Mphahama <83600286+Motion221100@users.noreply.github.com>
2026-01-21 22:16:56 +0200

    Update dim_date.sql

--- scripts/gold/dim_date.sql
+++ scripts/gold/dim_date.sql
@@ -5,7 +5,20 @@ SELECT DISTINCT
     MONTH(order_purchase_timestamp) AS month,
     QUARTER(order_purchase_timestamp) AS quarter,
     YEAR(order_purchase_timestamp) AS year,
-    DAYOFWEEK(order_purchase_timestamp) AS day_of_week
+    DAYOFWEEK(order_purchase_timestamp) AS day_of_week,
+    WEEK(order_purchase_timestamp) AS week_of_year,
+    DAYOFYEAR(order_purchase_timestamp) AS day_of_year,
+    DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS year_month,
+    CASE 
+        WHEN DAYOFWEEK(order_purchase_timestamp) IN (1,7) THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS day_type,
+    CASE 
+        WHEN MONTH(order_purchase_timestamp) IN (12,1,2) THEN 'Summer'
+        WHEN MONTH(order_purchase_timestamp) IN (3,4,5) THEN 'Autumn'
+        WHEN MONTH(order_purchase_timestamp) IN (6,7,8) THEN 'Winter'
+        WHEN MONTH(order_purchase_timestamp) IN (9,10,11) THEN 'Spring'
+    END AS season
 FROM silver.ord_orders;
 
 CREATE VIEW gold.dim_date_view AS
@@ -15,5 +28,18 @@ SELECT DISTINCT
     MONTH(order_purchase_timestamp) AS month,
     QUARTER(order_purchase_timestamp) AS quarter,
     YEAR(order_purchase_timestamp) AS year,
-    DAYOFWEEK(order_purchase_timestamp) AS day_of_week
+    DAYOFWEEK(order_purchase_timestamp) AS day_of_week,
+    WEEK(order_purchase_timestamp) AS week_of_year,
+    DAYOFYEAR(order_purchase_timestamp) AS day_of_year,
+    DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS year_month,
+    CASE 
+        WHEN DAYOFWEEK(order_purchase_timestamp) IN (1,7) THEN 'Weekend'
+        ELSE 'Weekday'
+    END AS day_type,
+    CASE 
+        WHEN MONTH(order_purchase_timestamp) IN (12,1,2) THEN 'Summer'
+        WHEN MONTH(order_purchase_timestamp) IN (3,4,5) THEN 'Autumn'
+        WHEN MONTH(order_purchase_timestamp) IN (6,7,8) THEN 'Winter'
+        WHEN MONTH(order_purchase_timestamp) IN (9,10,11) THEN 'Spring'
+    END AS season
 FROM silver.ord_orders;
