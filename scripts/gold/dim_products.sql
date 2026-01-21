e5aaa8652725a580a2c817396bb1378cf6b0ba78
Keith Mphahama <83600286+Motion221100@users.noreply.github.com>
2026-01-21 22:18:22 +0200

    Update dim_products.sql

--- scripts/gold/dim_products.sql
+++ scripts/gold/dim_products.sql
@@ -3,7 +3,19 @@ SELECT
     p.product_id,
     p.product_category_name,
     pnt.product_category_name_english AS product_name_translation,
-    AVG(oi.price) AS avg_price_sold
+    AVG(oi.price) AS avg_price_sold,
+    COUNT(DISTINCT oi.order_id) AS total_orders,
+    COUNT(DISTINCT oi.seller_id) AS unique_sellers,
+    SUM(oi.price) AS total_revenue,
+    SUM(oi.freight_value) AS total_freight_revenue,
+    COALESCE(AVG(p.product_photos_qty),0) AS avg_photos_qty,
+    COALESCE(AVG(p.product_weight_g),0) AS avg_weight_g,
+    COALESCE(AVG(p.product_description_length),0) AS avg_description_length,
+    CASE 
+        WHEN COUNT(DISTINCT oi.order_id) > 10 THEN 'High Demand'
+        WHEN COUNT(DISTINCT oi.order_id) BETWEEN 3 AND 10 THEN 'Medium Demand'
+        ELSE 'Low Demand'
+    END AS demand_category
 FROM silver.prod_products p
 LEFT JOIN silver.prod_category_name_translation pnt
     ON p.product_category_name = pnt.product_category_name
@@ -19,7 +31,19 @@ SELECT
     p.product_id,
     p.product_category_name,
     pnt.product_category_name_english AS product_name_translation,
-    AVG(oi.price) AS avg_price_sold
+    AVG(oi.price) AS avg_price_sold,
+    COUNT(DISTINCT oi.order_id) AS total_orders,
+    COUNT(DISTINCT oi.seller_id) AS unique_sellers,
+    SUM(oi.price) AS total_revenue,
+    SUM(oi.freight_value) AS total_freight_revenue,
+    COALESCE(AVG(p.product_photos_qty),0) AS avg_photos_qty,
+    COALESCE(AVG(p.product_weight_g),0) AS avg_weight_g,
+    COALESCE(AVG(p.product_description_length),0) AS avg_description_length,
+    CASE 
+        WHEN COUNT(DISTINCT oi.order_id) > 10 THEN 'High Demand'
+        WHEN COUNT(DISTINCT oi.order_id) BETWEEN 3 AND 10 THEN 'Medium Demand'
+        ELSE 'Low Demand'
+    END AS demand_category
 FROM silver.prod_products p
 LEFT JOIN silver.prod_category_name_translation pnt
     ON p.product_category_name = pnt.product_category_name
